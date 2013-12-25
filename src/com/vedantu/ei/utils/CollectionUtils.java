405d779f6472fd0ccbec2365245464f99dbe530f
Ujjawal Misra <ujjawal.misra@vedantu.com>
2013-12-25 19:36:31 +0530

    separated CollectionUtils

---
+++ src/com/vedantu/ei/utils/CollectionUtils.java
@@ -0,0 +1,13 @@
+package com.vedantu.ei.utils;
+
+import java.util.Collection;
+
+public class CollectionUtils {
+
+	public static <E> boolean isEmpty(Collection<E> inputList) {
+
+		return null == inputList || inputList.isEmpty();
+
+	}
+
+}
--- src/com/vedantu/ei/utils/StringUtils.java
+++ src/com/vedantu/ei/utils/StringUtils.java
@@ -1,7 +1,5 @@
 package com.vedantu.ei.utils;
 
-import java.util.Collection;
-
 public class StringUtils {
 
 	public static final String EMPTY = "";
@@ -9,12 +7,6 @@ public class StringUtils {
 	public static boolean isEmpty(String input) {
 
 		return null == input || input.isEmpty();
-
-	}
-
-	public static <E> boolean isEmpty(Collection<E> inputList) {
-
-		return null == inputList || inputList.isEmpty();
 
 	}
 }
