7cd9eeb094b35191607efc2ac56db5ea5367ef5d
Ujjawal Misra <ujjawal.misra@vedantu.com>
2014-01-09 13:13:34 +0530

    util function for checking isEmpty on map

--- src/com/vedantu/ei/utils/CollectionUtils.java
+++ src/com/vedantu/ei/utils/CollectionUtils.java
@@ -1,11 +1,16 @@
 package com.vedantu.ei.utils;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class CollectionUtils {
 
-	public static boolean isEmpty(Collection inputList) {
-		return null == inputList || inputList.isEmpty();
+	public static boolean isEmpty(Collection collection) {
+		return null == collection || collection.isEmpty();
+	}
+	
+	public static boolean isEmpty(Map map) {
+		return null == map || map.isEmpty();
 	}
 
 }
